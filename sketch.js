bef2b280752c6b06db216449b6819f8303ab677e
whitehatvrashab <ajithaveri@gmail.com>
2021-02-17 19:47:53 +0530

    added slingshot

--- sketch.js
+++ sketch.js
@@ -1,10 +1,12 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint=Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var slingshot;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -36,6 +38,9 @@ function setup(){
 
     bird = new Bird(100,100);
 
+    log6= new Log(230,180,80,PI/2);
+    slingshot=new Slingshot(bird.body,log6.body);
+
 }
 
 function draw(){
@@ -61,4 +66,7 @@ function draw(){
 
     bird.display();
     platform.display();
+
+    log6.display();
+    slingshot.display();
 }
